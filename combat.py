007de50080e8c08861a93f3f7d5f18f70a02ebc2
Emma Graves <emmagraves@Emmas-MacBook-Pro.local>
2018-02-27 10:43:44 -0500

    Combat system is now fully fuctional

--- combat.py
+++ combat.py
@@ -1,11 +1,14 @@
 from random import randint
 
+print("Welcome to the combat system! (If you don't attack or defend you'll forfeit a turn!)")
+
 #your stats
 herohp = 30
 herodamage = randint(5, 8)
 herodefense = 3
 looper = 1
 isdefense = False
+status = True
 
 #monster's stats
 monsterhp = 20
@@ -15,17 +18,15 @@ print("Your HP: {}".format(herohp))
 print("Monster HP: {}".format(monsterhp))
 
 
-status = True
-
 for i in range(looper):
     while status == True:
         choice = input("Attack or defend?")
         herohp = herohp - monsterdamage
-        if choice == "attack" or "Attack":
+        if choice.lower() == "attack":
             monsterhp = monsterhp - herodamage
-        if choice == "defend" or "Defend":
+            print("• You did {} damage!".format(herodamage))
+        elif choice.lower() == "defend":
             monsterdamage = monsterdamage -3
-        print("• You did {} damage!".format(herodamage))
         print("• The monster did {} damage!".format(monsterdamage))
         print("Your HP: {}".format(herohp))
         print("Monster HP: {}".format(monsterhp))
@@ -33,7 +34,11 @@ for i in range(looper):
         looper = +1
         herodamage = randint(3,7)
         monsterdamage = randint(4, 6)
-        if monsterhp <= 0:
+        if herohp <= 0 and monsterhp <= 0:
+            looper = 0
+            print("Oh no! You both got knocked out!")
+            break
+        elif monsterhp <= 0:
             looper = 0
             print("Good work, you won!")
             break
